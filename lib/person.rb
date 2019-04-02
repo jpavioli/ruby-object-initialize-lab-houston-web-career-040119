d2e4c884a698151fe4fc45c2aa2f69884c4ca0ca
Jusitn Avioli <justin.avioli@gmail.com>
2019-04-02 22:55:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,9 @@
 class Person 
+  #initialize #name with the persons name
   def initialize(name)
     @name = name
   end
-  
+  #add functionality to update the persons name using the #name= method 
   def name=(new_name)
     @name = new_name
   end
